8ee8c2937e1208352048c8c0603042b8d1644710
iremkrkaplan <iremkrkaplan@gmail.com>
2025-07-23 15:09:17 +0300

    Initial Commit

---
+++ Apple-Podcasts-CloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple-Podcasts-CloneApp
+//
+//  Created by irem karakaplan on 23.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
